df0636288a7a36f72d62b24c93e5f69cf47a4529
Gamma <96841421+ThisGamma@users.noreply.github.com>
2022-05-19 02:29:51 -0600

    05/19/2022 - Gamma
    MM/DD/YYYY - Author

---
+++ cliColors.py
@@ -0,0 +1,6 @@
+# Console line colors
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[33m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
---
+++ main.py
@@ -0,0 +1,11 @@
+# The following line will import the
+# Command Line Interface (CLI)
+import modules.cli as cli 
+
+def main():
+    #Configuración inicial
+    cli.config()
+    cli.menu()
+
+if __name__ == "__main__":
+    main()
